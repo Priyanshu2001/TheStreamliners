25ecafc07f594df6a936f487bc87e8f6bc2b4f70
Priyanshu2001 <prbansal.01@gmail.com>
2020-10-17 23:51:16 +0530

    modified

--- task2/GroceryCartItem.java
+++ task2/GroceryCartItem.java
@@ -13,12 +13,20 @@ public class GroceryCartItem extends GroceryItem{
         return (String.format("%s (Rs. %d x %f kg) = Rs. %f",name,pricePerKg,quantity,price));
     }
     public static float extractQuantity(String quantityStr){
-      String[] str = quantityStr.split(" ",quantityStr.length());
-      String replaceStr0= str[0].replace("kg","");
-      String replaceStr1= str[1].replace("g","");
-      int part1 = Integer.parseInt(replaceStr0);
-      int part2 = Integer.parseInt(replaceStr1);
-        return part1+(float)part2/1000;
+         float quant;
+      String quantitystr = quantityStr.replace("kg ",".")
+                                    .replace("kg",".")
+                                    .replace("g","");
+
+      if(!quantitystr.contains("."))
+          quantitystr= "." +quantitystr;
+
+        if (quantityStr.length()<=3)
+        quant=Float.parseFloat(quantitystr)/1000;
+        else
+        quant=Float.parseFloat(quantitystr);
+
+        return quant;
 
     }
     public static GroceryCartItem createNew(GroceryItem item, String quantityStr){
